97a8a950e638ba5a08633bec739b94ce199662e3
bewreks <bewreks@mail.ru>
2022-11-01 22:30:16 +0300

    add movetoward method

--- Assets/Scripts/Helpers/MathHelper.cs
+++ Assets/Scripts/Helpers/MathHelper.cs
@@ -1,4 +1,5 @@
 ﻿using Unity.Mathematics;
+using UnityEngine;
 
 namespace Helpers
 {
@@ -13,24 +14,14 @@ namespace Helpers
 			return angle * sign;
 		}
 		
-		// public static float AngleSigned(float3 from, float3 to, float3 axis)
-		// {
-		// 	float num1 = Angle(from, to);
-		// 	float num2 = from.y * to.z - from.z * to.y;
-		// 	float num3 = from.z * to.x - from.x * to.z;
-		// 	float num4 = from.x * to.y - from.y * to.x;
-		// 	float num5 = math.sign(axis.x * num2 +
-		// 	                       axis.y * num3 +
-		// 	                       axis.z * num4);
-		// 	return num1 * num5;
-		// }
-		//
-		// public static float Angle(float3 from, float3 to)
-		// {
-		// 	var num = math.sqrt(math.length(from) * math.length(to));
-		// 	return num < 1.0000000036274937E-15
-		// 		       ? 0.0f
-		// 		       : math.acos(math.clamp(math.dot(from, to) / num, -1f, 1f)) * 57.29578f;
-		// }
+		public static float3 MoveTowards(float3 current, float3 target, float maxDistanceDelta)
+		{
+			var delta  = target - current;
+			var sqdist = math.lengthsq(delta);
+			if (sqdist == 0 || sqdist <= maxDistanceDelta * maxDistanceDelta)
+				return target;
+			var dist = math.sqrt(sqdist);
+			return current + delta * (maxDistanceDelta / dist);
+		}
 	}
 }
